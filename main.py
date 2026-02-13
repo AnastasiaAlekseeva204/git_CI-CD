ccb4163142f3d44cb9c642591e93d8c337e4194b
Anastasia Alekseeva <1360nastya@mail.ru>
2026-02-13 11:07:52 +0300

    error_with_depend

--- main.py
+++ main.py
@@ -10,15 +10,14 @@ class Product(BaseModel):
     description: str
     created_at: str
 
-
-  app = FastAPI(title="E-Shop-СI-CD")
+app = FastAPI(title="E-Shop-СI-CD")
 
 with open(Path(__file__).parent / "shop.json", "r", encoding="utf-8") as f:
     PRODUCTS = json.load(f)
 
 @app.get("/products")
 async def get_products():
-    return PRODUCTS.
+    return PRODUCTS
 
 @app.get("/product/{pid}")
 async def get_product(pid: int):
