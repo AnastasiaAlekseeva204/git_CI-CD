b957a8726a8453ccd745669e1482056d28683679
Anastasia Alekseeva <1360nastya@mail.ru>
2026-02-13 11:02:01 +0300

    first_CI_ошибка

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ class Product(BaseModel):
     created_at: str
 
 
-app = FastAPI(title="E-Shop-СI-CD")
+  app = FastAPI(title="E-Shop-СI-CD")
 
 with open(Path(__file__).parent / "shop.json", "r", encoding="utf-8") as f:
     PRODUCTS = json.load(f)
 
 @app.get("/products")
 async def get_products():
-    return PRODUCTS
+    return PRODUCTS.
 
 @app.get("/product/{pid}")
 async def get_product(pid: int):
