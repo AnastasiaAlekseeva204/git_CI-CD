137d3565ac6f77a77d28d0398d6dc7581b485959
Anastasia Alekseeva <1360nastya@mail.ru>
2026-02-20 12:04:53 +0300

    fix5

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ import os
 import psycopg
 from psycopg.rows import dict_row
 
-
-
 DB_CONFIG = {
     "host": os.getenv("DB_HOST", "localhost"),
     "port": int(os.getenv("DB_PORT", "5433")),
@@ -17,8 +15,10 @@ DB_CONFIG = {
     "dbname": os.getenv("DB_NAME", "postgres"),
 }
 
+
 def get_db_connection():
     return psycopg.connect(**DB_CONFIG, row_factory=dict_row)
+
 
 app = FastAPI(title="E-Shop-СI-CD")
 
@@ -28,7 +28,9 @@ async def get_products():
     conn = get_db_connection()
     try:
         with conn.cursor() as cur:
-            cur.execute("SELECT id, name, price, description, created_at FROM video_cards ORDER BY id")
+            cur.execute(
+                "SELECT id, name, price, description, created_at FROM video_cards ORDER BY id"
+            )
             return [{**dict(r), "price": float(r["price"])} for r in cur.fetchall()]
     finally:
         conn.close()
